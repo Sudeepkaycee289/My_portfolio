7326b4670d2d4f71adff99627058697f01e9811b
Sudeepkaycee289 <sudipkc289@gmail.com>
2024-11-22 18:39:20 +0545

    Update UI

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ function hideSidebar() {
   sidebar.classList.remove('active'); // Remove 'active' class to hide the sidebar
 }
 
+// Js for Light / Dark Mode
+    const toggleButton = document.getElementById("toggleTheme");
+    const body = document.body;
+
+    toggleButton.addEventListener("click", () => {
+        // Toggle light mode class on the body
+        body.classList.toggle("light-mode");
+
+        // Change button icon based on the theme
+        if (body.classList.contains("light-mode")) {
+            toggleButton.textContent = "☽︎"; // Moon icon for dark mode
+        } else {
+            toggleButton.textContent = "☀︎"; // Sun icon for light mode
+        }
+    });
+
 
 // JS for Skills
 var tablinks = document.getElementsByClassName("tab-links");
